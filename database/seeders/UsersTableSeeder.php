e9504d62583abaecc1bb6cec2218845abf3e4ee5
Glino092 <78809664+Glino092@users.noreply.github.com>
2022-10-23 00:23:35 -0500

    Initial commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin Admin',
+            'email' => 'admin@argon.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('secret'),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'perfil' => 'admin'
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Gustavo Lino',
+            'email' => 'gl@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'created_at' => now(),
+            'updated_at' => now(),
+            'perfil' => 'admin'
+        ]);
+    }
+}
